311caeb77d37c7a4e8d9c0b5f1d3e5d5c1ae749b
EltonShinaider <eltonshinaider@hotmail.com>
2024-05-25 22:02:44 -0300

    inciando

--- java-autonomia-classes/src/MinhaClasse.java
+++ java-autonomia-classes/src/MinhaClasse.java
@@ -1,3 +1,17 @@
 public class MinhaClasse {
-    
+    public static void main(String[] args) {
+        System.out.println("Olá turma, sejam bem vindos!");
+
+
+        String primeiroNome = "Elton";
+        String segundoNome = "Shinaider";
+        
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
