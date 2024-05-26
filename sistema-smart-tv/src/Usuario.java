81487bc7d8efbeb6f848e6293b263c762528659d
EltonShinaider <eltonshinaider@hotmail.com>
2024-05-26 01:13:01 -0300

    novos dados

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,9 +3,25 @@ public class Usuario {
 
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual é: " + smartTv.volume);
+
+        System.out.println("Canal atual é: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual é: " + smartTv.canal);
+
+
         System.out.println("A TV está ligada? " + smartTv.ligada);
         System.out.println("Canal atual é: " + smartTv.canal);
         System.out.println("Volume atual é: " + smartTv.volume);
 
+        smartTv.ligar();
+        System.out.println("Novo status - TV ligada? " + smartTv.ligada);
+        
+        smartTv.desligar();
+        System.out.println("Novo status - TV ligada? " + smartTv.ligada);
     }
 }
